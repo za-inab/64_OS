0fdd7131fcf94bd51d2ee9c38378ac5900c304ae
Zainab Asif <66025262+za-inab@users.noreply.github.com>
2021-04-22 01:08:56 +0500

    Assignment

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,18 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("SEECS|SEECS  \t  SEECS|SEECS  \t  SEECS|SEECS  \t  SEECS|SEECS  \t  SEECS|SEECS \n");
+    print_str("E            \t  E            \t  E            \t  E            \t  E \n");
+    print_str("E            \t  E            \t  E            \t  E            \t  E \n");
+    print_str("C            \t  C            \t  C            \t  C            \t  C \n");
+    print_str("S            \t  S            \t  S            \t  S            \t  S \n");
+    print_str("SEECS|SEECS  \t  SEECS|SEECS  \t  SEECS|SEECS  \t  S            \t  SEECS|SEECS \n");
+    print_str("          E  \t  E            \t  E            \t  E            \t  E \n");
+    print_str("          E  \t  E            \t  E            \t  E            \t  E \n");
+    print_str("          C  \t  C            \t  C            \t  C            \t  C \n");
+    print_str("          S  \t  S            \t  S            \t  S            \t  S \n");
+    print_str("SEECS|SEECS  \t  SEECS|SEECS  \t  SEECS|SEECS  \t  SEECS|SEECS  \t  SEECS|SEECS \n");
+    
+}
